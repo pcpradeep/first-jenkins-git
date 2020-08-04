8f6378c74a82c9dcaeab854d9bb82c993d883b9b
pc pradeep <pc.pradeep6@gmail.com>
2020-08-04 20:14:50 +0530

    first git commit for jenkins

---
+++ src/com/mysimple/program/TestJava.java
@@ -0,0 +1,12 @@
+package com.mysimple.program;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Java from Jenkins");
+
+	}
+
+}
