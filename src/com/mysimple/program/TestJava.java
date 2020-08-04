ee5f9d186e9d17a6e2c531cf007f8a9716eb0d82
pc pradeep <pc.pradeep6@gmail.com>
2020-08-04 23:08:02 +0530

    Java file changed

--- src/com/mysimple/program/TestJava.java
+++ src/com/mysimple/program/TestJava.java
@@ -5,7 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Java from Jenkins");
+		System.out.println("Hello Java from Jenkins 2");
 
 	}
 
